28b2858dcaf9752b181bf9fcaf6d1fed2e4bb403
sangho <steve0312@naver.com>
2023-04-21 19:25:48 +0900

    add string about Third Test in PrTest.java file & using instruction add .

--- src/PrTest.java
+++ src/PrTest.java
@@ -2,5 +2,6 @@ public class PrTest {
     public static void main(String[] args) {
         System.out.println("Pull-request Test");
         System.out.println("Second Test");
+        System.out.println("Third Test");
     }
 }
